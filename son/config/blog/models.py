d04d9c5fd16c3fcf3686d08002bb8e8b266492f3
z3os <abolfazlmajidi1382@gmail.com>
2021-04-10 16:49:04 +0430

    name-1.17

--- son/config/blog/models.py
+++ son/config/blog/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from django.utils import timezone
-
+from extensions.utils import jalali_converter
 # Create your models here.
 class Article(models.Model):
     STATUS_CHOICES=(
@@ -23,3 +23,7 @@ class Article(models.Model):
 
     def __str__(self):
         return self.title
+
+    def jpublish(self):
+        return jalali_converter(self.publish)
+    jpublish.short_description = "زمان انتشار"
