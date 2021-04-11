30ca6eb06b68c0c093b59e816ea86dbc697f9f87
z3os <abolfazlmajidi1382@gmail.com>
2021-04-11 18:41:56 +0430

    18

--- son/config/blog/admin.py
+++ son/config/blog/admin.py
@@ -1,13 +1,22 @@
 from django.contrib import admin
-from .models import Article
+from .models import Article ,Category
 # Register your models here.
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('position','title','slug','status')
+    list_filter = (['status'])
+    search_fields = ('title','slug')
+
+
+admin.site.register(Category,CategoryAdmin)
+
+
+
 class ArticleAdmin(admin.ModelAdmin):
-    list_display = ('title','slug','jpublish','status')
+    list_display = ('title','slug','jpublish','status','category_to_str')
     list_filter = ('publish','status')
     search_fields = ('title','description')
     ordering = ['-status', '-publish']
 
-
-
-
+    def category_to_str(self, obj):
+        return "Categories"
 admin.site.register(Article,ArticleAdmin)
--- son/config/blog/models.py
+++ son/config/blog/models.py
@@ -2,6 +2,21 @@ from django.db import models
 from django.utils import timezone
 from extensions.utils import jalali_converter
 # Create your models here.
+class Category(models.Model):
+        title=models.CharField(max_length=100, verbose_name ="عنوان دسته بندی")
+        slug=models.SlugField(max_length=100,unique=True, verbose_name ="آدرس دسته بندی")
+        status = models.BooleanField(default=True, verbose_name ="آیا نمایش داده شود ؟")
+        position = models.IntegerField(verbose_name = "پوزیشن")
+
+        class Meta:
+            verbose_name = "دسته بندی"
+            verbose_name_plural = "دسته بندی ها"
+            ordering = ['position']
+
+        def __str__(self):
+            return self.title
+
+
 class Article(models.Model):
     STATUS_CHOICES=(
         ('d', 'پیش نویس'),
@@ -10,6 +25,7 @@ class Article(models.Model):
     title=models.CharField(max_length=100, verbose_name ="عنوان مقاله")
     slug=models.SlugField(max_length=100,unique=True, verbose_name ="آدرس مقاله")
     description = models.TextField(verbose_name ="متن")
+    categoty=models.ManyToManyField(Category, verbose_name="دسته بندی")
     thumbnail = models.ImageField(upload_to="images", verbose_name ="تصویر")
     publish = models.DateTimeField(default=timezone.now, verbose_name ="زمان انتشار")
     created = models.DateTimeField(auto_now_add=True)
